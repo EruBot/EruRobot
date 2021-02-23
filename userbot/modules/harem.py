910fa40d5c6fafc1dc39c02b7bb4e393faa087bf
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-23 08:56:19 +0700

    Update harem.py

--- userbot/modules/harem.py
+++ userbot/modules/harem.py
@@ -8,11 +8,27 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^.w(?: |$)(.*)')
+@register(outgoing=True, pattern='^mash(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(0)
-    await typew.edit("/protecc shuten martha altera Atalante penthesilea quetzalcoatl jinako chevalier artoria qin rhyme mata shiki yi medb osakabehime kiyohime Nightingale yu ke Hokusai Carmilla Mysterious Mae drake europa boudica chacha circe medea murasaki kiara ishtar jeanne jane bradamante brynhildr tomoe gozen irisviel von katou medusa parvati orion stheno euryale nero kingprotea hassan yang abigail illyasviel ereshkigal bb charlotte tamamo chiyome minamoto wu helena ibaraki okita oda mash astolfo mordred nitocris gorgon paul benienma mecha elisabeth gray jack marie anastasia passionlip scheherazade semiramis salome lakshmibai valkyrie anne da fujino nagao")
+    await typew.edit("/protecc shuten martha altera Atalante penthesilea quetzalcoatl jinako chevalier artoria qin rhyme mata")
+    sleep(0)
+    await typew.edit("shiki yi medb osakabehime kiyohime Nightingale yu ke Hokusai Carmilla Mysterious Mae drake europa")
+    sleep(0)
+    await typew.edit("boudica chacha circe medea murasaki kiara ishtar jeanne jane bradamante brynhildr tomoe gozen")
+    sleep(0)
+    await typew.edit("irisviel von katou medusa parvati orion stheno euryale nero kingprotea hassan yang abigail")
+    sleep(0)
+    await typew.edit("illyasviel ereshkigal bb charlotte tamamo chiyome minamoto wu helena ibaraki okita oda mash astolfo")
+    sleep(0)
+    await typew.edit("mordred nitocris gorgon paul benienma mecha elisabeth gray jack marie anastasia passionlip")
+    sleep(0)
+    await typew.edit("scheherazade semiramis salome lakshmibai valkyrie anne da fujino nagao")
+    sleep(0)
+    await typew.edit("udah")
+    sleep(0)
+    await typew.edit("udahhh")
 # Owner @erruuu - @salama219
 
 
@@ -24,20 +40,20 @@ async def typewriter(typew):
 # Owner @erruuu - @salama219
 
 
-@register(outgoing=True, pattern='^.h(?: |$)(.*)')
+@register(outgoing=True, pattern='^nana(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(0)
-    await typew.edit("/protecc hikari hikari oono kirigaya mio atago gaen araragi chisa yuuna izumi izuna nao ruka rikka erice erika chihiro nakano uesugi yui yukino hanyuu medusa mito mii boudica akari tomoe mami papi kagome beatrice eru kyons sister sae cabashira ichinose rias kanao akeno futaba koneko meteora aoko kyouko shinku youmu ryouko asuna haruhi natsumi lelei margaret")
-# Owner @erruuu @salama219
+    await typew.edit("/protecc hikari oono kirigaya mio atago gaen araragi chisa yuuna izumi izuna nao ruka rikka erice erika chihiro nakano uesugi yui yukino hanyuu medusa mito mii boudica akari tomoe mami papi kagome beatrice eru kyons sister sae cabashira ichinose rias kanao akeno futaba koneko meteora aoko kyouko shinku youmu ryouko asuna haruhi natsumi lelei margaret")
+# Owner @erruuu - @salama219
 
 
 CMD_HELP.update({
     "harem":
-    "`s`\
-\nUsage: Protecc servant (beta).\
-\n\n`h`\
-\nUsage: Protecc loli (beta).\
+    "`mash`\
+\nUsage: Protecc servant (v.2).\
+\n\n`nana`\
+\nUsage: Protecc loli (v.1).\
 \n\n`xx`\
 \nUsage: Protecc servant char mysterious."
 })
